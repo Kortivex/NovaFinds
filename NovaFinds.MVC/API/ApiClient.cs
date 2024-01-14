4d4e105152ec7b9f8ad8aa3a050c99f72b7092b2
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-01-14 20:18:44 +0100

    change(main): add api client & endpoints

---
+++ NovaFinds.MVC/API/ApiClient.cs
@@ -0,0 +1,31 @@
+﻿namespace NovaFinds.MVC.API
+{
+    using System.Net.Http.Headers;
+    using System.Text.Json;
+
+    public class ApiClient(IConfiguration config)
+    {
+        private string? Url { get; set; } = config.GetSection("Config").GetSection("Apis").GetSection("NovaFinds").Value;
+
+        private string? ApiKey { get; set; } = config.GetSection("Config").GetSection("ApiKeys").GetSection("NovaFinds").Value;
+
+        private HttpClient GenerateHttpClient()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            httpClient.DefaultRequestHeaders.Add("X-Api-Key", this.ApiKey);
+            return httpClient;
+        }
+
+        public async Task<T?> Get<T>(string action)
+        {
+            var httpClient = GenerateHttpClient();
+            var result = await httpClient.GetAsync(this.Url + action);
+            result.EnsureSuccessStatusCode();
+            var resultContent = result.Content.ReadAsStringAsync().Result;
+            return JsonSerializer.Deserialize<T>(resultContent);
+        }
+
+    }
+}
---
+++ NovaFinds.MVC/API/ApiEndPoints.cs
@@ -0,0 +1,27 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ApiEndPoints.cs" company="">
+//
+// </copyright>
+// <summary>
+//   The ApiEndPoints const.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NovaFinds.MVC.API
+{
+    /// <summary>
+    /// Static class with NovaFinds REST API endpoints.
+    /// </summary>
+    public static class ApiEndPoints
+    {
+        /// <summary>
+        /// GET /products
+        /// </summary>
+        public const string GetProducts = "/products/?size={0}&sortBy={1}";
+        
+        /// <summary>
+        /// GET /categories
+        /// </summary>
+        public const string GetCategories = "/categories";
+    }
+}
