39c5fbe71a166cf5687fce6c6c2089f4ac66f6ae
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-01-14 20:17:40 +0100

    change(main): add category menu component

---
+++ NovaFinds.MVC/Components/CategoryMenu.cs
@@ -0,0 +1,39 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="CategoryMenu.cs" company="">
+//
+// </copyright>
+// <summary>
+//   Defines the CategoryMenu type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NovaFinds.MVC.Components
+{
+    using API;
+    using DTOs;
+    using IFR.Logger;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// The category menu.
+    /// </summary>
+    public class CategoryMenu(IConfiguration configuration) : ViewComponent
+    {
+        private ApiClient _apiClient = new(configuration);
+
+        /// <summary>
+        /// The invoke async.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Task"/>.
+        /// </returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            Logger.Debug("Init Component");
+            var categories = await _apiClient.Get<IEnumerable<CategoryDto>>(ApiEndPoints.GetCategories);
+            return View(categories);
+        }
+    }
+}
