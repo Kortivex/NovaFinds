291690ce73e71d8815c7c59c90bf5838f9c7ec6f
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-01-18 21:39:38 +0100

    change(main): update api endpoints

--- NovaFinds.MVC/API/ApiEndPoints.cs
+++ NovaFinds.MVC/API/ApiEndPoints.cs
@@ -14,14 +14,35 @@ namespace NovaFinds.MVC.API
     /// </summary>
     public static class ApiEndPoints
     {
+        
         /// <summary>
         /// GET /products
         /// </summary>
-        public const string GetProducts = "/products/?size={0}&sortBy={1}";
+        public const string GetProducts = "/products";
+        
+        /// <summary>
+        /// GET /products with sort filters.
+        /// </summary>
+        public const string GetProductsSortFilters = "/products/?size={0}&sortBy={1}";
+        
+        /// <summary>
+        /// GET /products with category and sort filters.
+        /// </summary>
+        public const string GetProductsCategorySortFilter = "/products/?category={0}&size={1}&page={2}";
         
         /// <summary>
         /// GET /categories
         /// </summary>
         public const string GetCategories = "/categories";
+        
+        /// <summary>
+        /// GET /categories with sort filters.
+        /// </summary>
+        public const string GetCategoriesSortFilters = "/categories/?size={0}&page={1}";
+        
+        /// <summary>
+        /// GET /categories/:id
+        /// </summary>
+        public const string GetCategory = "/categories/{0}";
     }
 }
