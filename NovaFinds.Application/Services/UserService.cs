0bea821c828c45a95dc51917cd24f14a0aaeb1bb
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-01-26 20:24:18 +0100

    change(main): add get user by name & create user with password

--- NovaFinds.Application/Services/UserService.cs
+++ NovaFinds.Application/Services/UserService.cs
@@ -11,18 +11,58 @@ namespace NovaFinds.Application.Services
 {
     using CORE.Contracts;
     using CORE.Domain;
+    using Microsoft.AspNetCore.Identity;
 
     /// <summary>
     /// The user service.
     /// </summary>
     public class UserService : Service<User>, IUserRepository
     {
+        private readonly UserManager<User> _userManager;
+
         /// <summary>
         /// Initializes a new instance of the <see cref="UserService"/> class.
         /// </summary>
         /// <param name="context">
         /// The context.
         /// </param>
-        public UserService(IDbContext context) : base(context) {}
+        /// <param name="userManager">
+        /// The UserManager from Identity.
+        /// </param>
+        public UserService(IDbContext context, UserManager<User> userManager) : base(context)
+        {
+            _userManager = userManager;
+        }
+
+        /// <summary>
+        /// Get the specified <paramref name="username"/> in the backing store.
+        /// </summary>
+        /// <param name="username">
+        /// The username.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IdentityResult"/>.
+        /// </returns>
+        public async Task<User?> GetByUserNameAsync(string username)
+        {
+            return await _userManager.FindByNameAsync(username);
+        }
+
+        /// <summary>
+        /// Creates the specified <paramref name="user"/> in the backing store with given password.
+        /// </summary>
+        /// <param name="user">
+        /// The user.
+        /// </param>
+        /// <param name="password">
+        /// The user password.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IdentityResult"/>.
+        /// </returns>
+        public async Task<IdentityResult> CreateUserAsync(User user, string password)
+        {
+            return await _userManager.CreateAsync(user, password);
+        }
     }
 }
