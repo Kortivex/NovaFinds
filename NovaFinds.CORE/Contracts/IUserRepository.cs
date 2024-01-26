ac297fccec95db8979a570f2a4541c9a2bc37eb7
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-01-26 20:24:37 +0100

    change(main): add get user by name & create user with password contract

--- NovaFinds.CORE/Contracts/IUserRepository.cs
+++ NovaFinds.CORE/Contracts/IUserRepository.cs
@@ -10,11 +10,37 @@
 namespace NovaFinds.CORE.Contracts
 {
     using Domain;
+    using Microsoft.AspNetCore.Identity;
 
     /// <summary>
     /// The UserRepository interface.
     /// </summary>
     public interface IUserRepository : IRepository<User>
     {
+
+        /// <summary>
+        /// Get the specified <paramref name="username"/> in the backing store.
+        /// </summary>
+        /// <param name="username">
+        /// The username.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IdentityResult"/>.
+        /// </returns>
+        public Task<User?> GetByUserNameAsync(string username);
+        
+        /// <summary>
+        /// Creates the specified <paramref name="user"/> in the backing store with given password.
+        /// </summary>
+        /// <param name="user">
+        /// The user.
+        /// </param>
+        /// <param name="password">
+        /// The user password.
+        /// </param>
+        /// <returns>
+        /// The <see cref="IdentityResult"/>.
+        /// </returns>
+        public Task<IdentityResult> CreateUserAsync(User user, string password);
     }
 }
