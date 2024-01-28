d9436573a6a3fc4d9a2148458f9d530fc506b8f8
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-01-28 16:43:15 +0100

    change(main): add access denied page

---
+++ NovaFinds.MVC/Areas/Identity/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,24 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AccessDenied.cshtml.cs" company="">
+//
+// </copyright>
+// <summary>
+//   Defines the AccessDeniedModel type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace NovaFinds.MVC.Areas.Identity.Pages.Account
+{
+    using Microsoft.AspNetCore.Mvc.RazorPages;
+
+    /// <summary>
+    /// The access denied model.
+    /// </summary>
+    public class AccessDeniedModel : PageModel
+    {
+        /// <summary>
+        /// The on get method.
+        /// </summary>
+        public void OnGet() {}
+    }
+}
