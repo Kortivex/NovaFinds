6f01eb1f635230ffc782ae2022fd1f61f8854a00
Juan Daniel Díaz Matamoros <johnd91@gmail.com>
2024-01-28 16:16:24 +0100

    change(db): add new migr.

--- NovaFinds.DAL/Migrations/20240128122833_add[_user_validation_fields.Designer.cs
+++ NovaFinds.DAL/Migrations/20240128122833_add[_user_validation_fields.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NovaFinds.DAL.Context;
 
@@ -11,9 +12,11 @@ using NovaFinds.DAL.Context;
 namespace NovaFinds.DAL.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240128122833_add[_user_validation_fields")]
+    partial class add_user_validation_fields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
